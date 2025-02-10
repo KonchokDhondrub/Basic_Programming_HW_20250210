3502e6d0aced65c7d7cb692a8e5186a9f4713498
kd <erm.melnik@gmail.com>
2025-02-10 19:50:04 +0100

    Added variables, constructor, fly() method.

--- src/Aerostat.java
+++ src/Aerostat.java
@@ -1,3 +1,18 @@
-public class Aerostat {
+public class Aerostat implements Flyable{
     private String name;
+    private int capacity;
+    private int flyDistance;
+
+    public Aerostat(String name, int capacity, int flyDistance) {
+        this.name = name;
+        this.capacity = capacity;
+        this.flyDistance = flyDistance;
+    }
+
+    @Override
+    public String fly(){
+        return String.format("%s, вместимость: %d человек, дальность полёта: %d км \n" +
+                        "Аэростат наполняется горячим воздухом и поднимается вверх.",
+                name, capacity, flyDistance);
+    }
 }
--- src/Aircraft.java
+++ src/Aircraft.java
@@ -1,4 +1,18 @@
-public class Aircraft {
+public class Aircraft implements Flyable{
     private String name;
     private int capacity;
+    private int flyDistance;
+
+    public Aircraft(String name, int capacity, int flyDistance) {
+        this.name = name;
+        this.capacity = capacity;
+        this.flyDistance = flyDistance;
+    }
+
+    @Override
+    public String fly(){
+        return String.format("%s, вместимость: %d человек, дальность полёта: %d км \n" +
+                        "Самолет взлетает с полосы и поднимается в небо.",
+                name, capacity, flyDistance);
+    }
 }
