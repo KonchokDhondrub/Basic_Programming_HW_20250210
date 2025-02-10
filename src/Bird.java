a01e5b73202b5056ab1ced042d96ee0f8f3e4839
kd <erm.melnik@gmail.com>
2025-02-10 19:49:43 +0100

    Added variables, constructor, getters, fly() method.

--- src/Bird.java
+++ src/Bird.java
@@ -1,2 +1,35 @@
-public class Bird {
+public abstract class Bird {
+    private String breed;
+    private int height;
+    private int weight;
+    private int wingspan;
+    private int flyDistance;
+
+    public Bird(String breed, int height, int weight, int wingspan, int flyDistance) {
+        this.breed = breed;
+        this.height = height;
+        this.weight = weight;
+        this.wingspan = wingspan;
+        this.flyDistance = flyDistance;
+    }
+
+    public String getBreed() {
+        return breed;
+    }
+    public int getHeight() {
+        return height;
+    }
+    public int getWeight() {
+        return weight;
+    }
+    public int getWingspan() {
+        return wingspan;
+    }
+    public int getFlyDistance() {
+        return flyDistance;
+    }
+
+    public String fly(){
+        return "Птица машет крыльями и летит.";
+    }
 }
