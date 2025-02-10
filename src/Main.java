0cdb4b605c79977f5b30d5d9ee8d757462a5f4c9
kd <erm.melnik@gmail.com>
2025-02-10 19:48:33 +0100

    Created List with Objects. Added print.

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,15 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        List<Flyable> flyables = new ArrayList<>();
+        flyables.add(new Duck("Egyptian goose", 70, 2, 145, 1200));
+        flyables.add(new Aircraft("Boeing 777", 368, 15000));
+        flyables.add(new Aerostat("Zeppelin NT D-LZZF", 12, 680));
+
+        for (Flyable flyable : flyables) {
+            System.out.println(flyable.fly());
+        }
     }
 }
