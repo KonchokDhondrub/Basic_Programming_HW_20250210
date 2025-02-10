32668b58071597ffe2ce492187edfd2ed9c973f1
kd <erm.melnik@gmail.com>
2025-02-10 19:50:13 +0100

    Added constructor, fly() method.

--- src/Duck.java
+++ src/Duck.java
@@ -1,4 +1,13 @@
-public class Duck {
-    private String name;
-    private String breed;
+public class Duck extends Bird implements Flyable{
+
+    public Duck(String breed, int height, int weight, int wingspan, int flyDistance) {
+        super(breed, height, weight, wingspan, flyDistance);
+    }
+
+    @Override
+    public String fly(){
+        return String.format("%s, высота: %d см, вес: %d кг, размах крыльев: %d см, дальность полёта: %d км \n" +
+                        "Утка машет крыльями и летит.",
+                getBreed(), getHeight(), getWeight(), getWingspan(), getFlyDistance());
+    }
 }
